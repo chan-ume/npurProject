3a5377675a237f7d94caac3c05e19a838afcd90f
chan-ume <chan-ume@users.noreply.github.com>
2017-08-24 12:37:24 +0900

    add ui

---
+++ Shiny_change_color/ui.R
@@ -0,0 +1,10 @@
+library(shiny)
+
+shinyUI(
+  
+  fluidPage(    
+    selectInput("sel",label = "col",choices = colnames(iris)[2:4]),
+    selectInput("color",label = "col",choices = c("red", "black", "blue", "green")),
+    plotOutput("plot")
+  )
+)
