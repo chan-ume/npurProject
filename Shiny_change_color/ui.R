972fd8d6627671226f3fda12aac78b0aa6066c0a
chan-ume <chan-ume@users.noreply.github.com>
2017-08-25 14:36:35 +0900

    ダウンロードボタン追加

--- Shiny_change_color/ui.R
+++ Shiny_change_color/ui.R
@@ -1,10 +1,13 @@
 library(shiny)
+library(rJava)
+library(ReporteRsjars)
 
 shinyUI(
   
   fluidPage(    
     selectInput("sel",label = "col",choices = colnames(iris)[2:4]),
     selectInput("color",label = "col",choices = c("red", "black", "blue", "green")),
-    plotOutput("plot")
+    plotOutput("plot"),
+    downloadButton('downloadData', 'Download')
   )
 )
