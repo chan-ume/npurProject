625cc6dd1b3d1695ed68b8c2a72dbcce41d887fa
chan-ume <chan-ume@users.noreply.github.com>
2017-11-27 00:47:04 +0900

    modify server.r

--- shiny_drag_and_drop/server.R
+++ shiny_drag_and_drop/server.R
@@ -2,6 +2,6 @@ server = function(input, output, session) {
   observeEvent(input$mydata, {
     name = names(input$mydata)
     csv_file = reactive(read.csv(text=input$mydata[[name]]))
-    output$table = renderTable(csv_file()[1,])
+    output$table = renderTable(csv_file())
   })
 }
